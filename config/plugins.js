0a1c86381f8a76d018e4357f92325dc3de41b76b
drsfieldsIMA <57874229+drsfieldsIMA@users.noreply.github.com>
2021-11-17 13:03:53 +0100

    initial

--- config/plugins.js
+++ config/plugins.js
@@ -3,9 +3,9 @@ module.exports = ({ env }) => ({
   upload: {
     provider: "cloudinary",
     providerOptions: {
-      cloud_name: env("noroff-as"),
-      api_key: env("134344697661213"),
-      api_secret: env("zzG_kQ0rjfAzatV_I2lMhDHBDak"),
+      cloud_name: "noroff-as",
+      api_key: "134344697661213",
+      api_secret: "zzG_kQ0rjfAzatV_I2lMhDHBDak",
     },
   },
   // ...
